e671af558db1f13f53336bf6f9a07c48c353629f
Qiancheng Sun <sunq11@udayton.edu>
2022-03-02 10:56:37 -0500

    Monte Carlo

--- Assignment_2.py
+++ Assignment_2.py
@@ -5,7 +5,7 @@
 import numpy as np
 from scipy.stats import t
 from scipy.stats import norm
-import matplotlib as plt
+import matplotlib.pyplot as plt
 #%%
 # Monte-Carlo Simulation
 def confidence_interval(x, confidence):
@@ -175,24 +175,112 @@ def bs_put(S, K, T, r, sigma):
     return put_option
 # %% Question 2
 # Use the Black-Scholes formula to price European all and put options
+# Part A
 maturity = 1 # T
 interest_rate = 0.07 # r
 volatility = 0.35 # sigma
 strike_price = 100 # K
 stock_price = np.array([80,85,90,95,98,100,102,105,110,115,120])  # S 
+# create empty list
+call_price_list = []
+put_price_list = []
+# calculate the call price/ put price for each stock price
+for i in range(0, len(stock_price)):
+    call_price = bs_call(S= stock_price[i],
+                        K= strike_price,
+                        T= maturity,
+                        r= interest_rate,
+                        sigma= volatility)
+    call_price_list.append(call_price)
+    put_price = bs_put(S= stock_price[i],
+                        K= strike_price,
+                        T= maturity,
+                        r= interest_rate,
+                        sigma= volatility)  
+    put_price_list.append(put_price)           
+
+# plot the call price
+x = stock_price
+y = call_price_list
+# figure define
+figure = plt.figure()
+ax = plt.axes()
+ax.plot(x,y)
+plt.title("Call Price")
+plt.xlabel("stock price")
+plt.ylabel("call price")
+plt.show()
+plt.savefig(r"/Users/qianchengsun/Desktop/Computational_Finance/Assignments/call_price.png")
+# plot the call price
+x = stock_price
+y = put_price_list
+# figure define
+figure = plt.figure()
+ax = plt.axes()
+ax.plot(x,y)
+plt.title("Put Price")
+plt.xlabel("stock price")
+plt.ylabel("Put price")
+plt.show()
+plt.savefig(r"/Users/qianchengsun/Desktop/Computational_Finance/Assignments/put_price.png")
+# %%
+# Part B
+
+#%%
+# Question 3 slove heat equation by explicit finite difference method
+# define heat explicit function
+def heat_explicit(delta_x, delta_t, t_max):
+    N = np.round(1 / delta_x)
+    N = int(N)
+    M = np.round(t_max / delta_t)
+    M = int(M)
+    sol = np.zeros(shape = (N + 1, M + 1))
+    rho = delta_t / (delta_x ** 2)
+    rho2 = 1 - 2 * rho
+    # devide 0 to 1 based on delta_x
+    vetx = np.linspace(0,1, int(1/delta_x) + 1)
+
+    for i in range(1, int(np.ceil((N + 1) / 2))): # from 2 to 6
+        sol[i,0] = 2 * vetx[i - 1]
+        sol[N + 1 - i, 0] = sol[i,0]
+
+    for j in range(0, M - 1):
+        for k in range(1, N - 1):
+            sol[k, j + 1 ] = rho * sol[k - 1, j] + rho2 * sol[k,j] + rho * sol[k + 1, j]
+
+    return sol
+#%%
+# define variables
+delta_x = 0.1
+delta_t = 0.001
+t_max = delta_t * 100
+sol = heat_explicit(delta_x = delta_x,delta_t = delta_t,t_max = t_max)
+# plot the result
+x = np.linspace(0,1, int(1/delta_x) + 1)
+y = sol[:,0]
+figure = plt.figure()
+ax = plt.axes()
+ax.plot(x,y)
+plt.show()
+#%%
+x = np.linspace(0,1, int(1/delta_x) + 1)
+y = sol[:,10]
+figure = plt.figure()
+ax = plt.axes()
+ax.plot(x,y)
+plt.show()
+x = np.linspace(0,1, int(1/delta_x) + 1)
+y = sol[:,50]
+figure = plt.figure()
+ax = plt.axes()
+ax.plot(x,y)
+plt.show()
+x = np.linspace(0,1, int(1/delta_x) + 1)
+y = sol[:,100]
+figure = plt.figure()
+ax = plt.axes()
+ax.plot(x,y)
+plt.show()
 
 
-
-
-
-call_price = bs_call(S= 80,
-                    K= 100,
-                    T= 1,
-                    r= 0.07,
-                    sigma= 0.35)
-put_price = bs_put(S= 80,
-                    K= 100,
-                    T= 1,
-                    r= 0.07,
-                    sigma= 0.35)                    
 # %%
